593696824a827d3ea00d1d6b3eb970aefb7cfcf3
Ajith Ranka <ajithranka@users.noreply.github.com>
2016-02-25 12:43:49 +0530

    Add form validation example

--- examples/basic-examples/index.js
+++ examples/basic-examples/index.js
@@ -99,7 +99,7 @@ import dom from 'frpjs/dom'
     }
 }
 
-// Example 8: Add
+// Example 8: Add two numbers
 {
     const input1$ = frp.compose(
         dom.createEventStream('#ex8-input1', 'input'),
@@ -122,4 +122,94 @@ import dom from 'frpjs/dom'
     )
 
     inputs$(input => dom.select('#ex8-label').textContent = frp.snapshot(totalValue))
+}
+
+// Example 9: Form validation
+{
+    // Validation functions
+    const validators = {
+        "name": function(value) {
+            const name = value.trim()
+
+            if (name.length == 0)
+                return "Required"
+            if (/([^A-Za-z\s])/.test(name))
+                return "Cannot contains special characters"
+            if (name.indexOf(' ') < 0)
+                return "Please enter your full name"
+            return ""
+        },
+
+        "quantity": function(value) {
+            const number = parseInt(value)
+
+            if (number < 1 || number > 3)
+                return "Choose a number between 1 and 3"
+            return ""
+        },
+
+        "email": function(value) {
+            const email = value.trim()
+
+            if (/\S+@\S+\.\S+/.test(email))
+                return ""
+            return "Please enter a valid email address"
+        }
+    }
+
+    function allValidationsPassed(events) {
+        let validations = []
+        for(let key in events) {
+            if (typeof events[key] == "object")
+                validations.push(events[key].valid)
+        }
+        return validations.length == 3 &&
+               validations.reduce((a, b) => a && b)
+    }
+
+    // Input stream
+    const inputs$ = frp.compose(
+        dom.createEventStream('#name', 'input'),
+        frp.merge(dom.createEventStream('#email', 'change')),
+        frp.merge(dom.createEventStream('#quantity', 'input')),
+
+        frp.map(event => ({
+            element: event.target,
+            name: event.target.name,
+            value: event.target.value,
+            validator: validators[event.target.name]
+        })),
+
+        frp.map(event => {
+            event.errorMessage = event.validator(event.value),
+            event.valid = (event.errorMessage == "") ? true : false
+
+            return event
+        }),
+
+        frp.fold((accumulator, currentEvent) => {
+            accumulator[currentEvent.name] = currentEvent
+            accumulator.current = currentEvent.name
+            accumulator.allValid = allValidationsPassed(accumulator)
+
+            return accumulator
+        }, {})
+    )
+
+    // Activation
+    inputs$(events => {
+        const currentEvent     = events[events.current]
+        const formGroupElement = currentEvent.element.parentElement
+        const errorSpanElement = currentEvent.element.nextElementSibling
+
+        if (currentEvent.valid == false) {
+            dom.addClass(formGroupElement, 'has-error')
+            errorSpanElement.textContent = currentEvent.errorMessage
+        } else {
+            dom.removeClass(formGroupElement, 'has-error')
+            errorSpanElement.textContent = ""
+        }
+
+        dom.select('#submit').disabled = !events.allValid
+    })
 }
